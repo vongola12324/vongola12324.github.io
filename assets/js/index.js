8f89c0ba793c92d8328acedbfc8644b019e4643c
Vongola <vongola12324@coder.tw>
2016-11-29 00:32:48 +0800

    Add avatar and random

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+randomAvatar();
+
+function randomAvatar () {
+  const dir = "/assets/img/avatars/Avatar";
+  const amount = 13;
+  const choice = Math.floor((Math.random() * amount+1));
+  $('#avatar').attr('src', dir+('0'+choice).slice(-2)+'.jpg');
+}
