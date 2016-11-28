3cce2639b2456b8c5900c14c6cf02dc56d3c07ef
Vongola <vongola12324@coder.tw>
2016-11-29 00:40:41 +0800

    Fix typo

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 randomAvatar();
 
 function randomAvatar () {
-  const dir = "/assets/img/avatars/Avatar";
+  const dir = "assets/img/avatars/Avatar";
   const amount = 13;
   const choice = Math.floor((Math.random() * amount+1));
   $('#avatar').attr('src', dir+('0'+choice).slice(-2)+'.jpg');
