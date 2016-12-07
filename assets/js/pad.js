5f957c5105cbf0f70ab31ed4119efaca2e6323aa
vongola12324 <vongola12324@coder.tw>
2016-12-07 19:48:15 +0800

    Split timer out from pad

--- assets/js/pad.js
+++ assets/js/pad.js
@@ -1,287 +1,81 @@
-function init () {
-  let strUrl = location.search;
-  let getPara, ParaVal;
-  let aryPara = [];
-  let timeInv;
-  let start_button = document.getElementById("start");
-  let stop_button = document.getElementById("stop");
+function init() {
+    let strUrl = location.search;
+    let getPara, ParaVal;
+    let aryPara = [];
+    let textArea = document.getElementById("editable");
+    let timeInv;
+    let start_button = document.getElementById("start");
+    let stop_button = document.getElementById("stop");
 
-  if (strUrl.indexOf("?") != -1) {
-    let getSearch = strUrl.split("?");
-    getPara = getSearch[ 1 ].split("&");
-    for (let i = 0; i < getPara.length; i++) {
-      ParaVal = getPara[ i ].split("=");
-      aryPara.push(ParaVal[ 0 ]);
-      aryPara[ ParaVal[ 0 ] ] = decodeURI(ParaVal[ 1 ]);
-    }
-
-    // Help
-    if ("help" in aryPara) {
-      swal({
-        title: "Help!",
-        text: "<h3>Here are some useful feature:</h3><br>" +
-        "<table>" +
-        "<thead><tr><th>Key</th><th>Value</th><th>Function</th></tr></thead>" +
-        "<tbody>" +
-        "<tr><td>help</td><td>(null)</td><td>Open this page.</td></tr>" +
-        "<tr><td>text</td><td>(text)</td><td>Open an editable pad with (text)</td></tr>" +
-        "<tr><td>clock</td><td>(null)</td><td>Open a clock.</td></tr>" +
-        "<tr><td>timing</td><td>(null)</td><td>Open a timer.</td></tr>" +
-        "<tr><td>countdown</td><td>(?d?h?m?s)</td><td>Open a countdown timer.</td></tr>" +
-        "<tr><td>image</td><td>(url)</td><td>Open an image pad with the specify image.</td></tr>" +
-        "</tbody>" +
-        "</table>",
-        html: true,
-        type: "info",
-        confirmButtonText: "Try it!"
-      });
-    } else {
-      // Add feature here
-
-      // C8763
-      if ("C8763" in aryPara) {
-        let url = "http://i.imgur.com/tqVvt49.gif";
-        document.getElementById("editable").innerHTML = "<img src=\"" + url + "\" style='width: 100%; height: auto;'/>"
-      }
-
-      // Image Pad
-      if ("image" in aryPara) {
-        let imgUrl = decodeURI(aryPara[ "image" ]);
-        imgUrl = imgUrl.replace(/%3A/g, ':').replace(/%2F/g, '/');
-        if (imgUrl != "undefined") {
-          document.getElementById("editable").innerHTML = "<img src=\"" + imgUrl + "\" style='width: 100%; height: auto;'/>"
-        } else {
-          document.getElementById("editable").innerText = "404<br>Image Not Found!";
+    if (strUrl.indexOf("?") != -1) {
+        let getSearch = strUrl.split("?");
+        getPara = getSearch[1].split("&");
+        for (let i = 0; i < getPara.length; i++) {
+            ParaVal = getPara[i].split("=");
+            aryPara.push(ParaVal[0]);
+            aryPara[ParaVal[0]] = decodeURI(ParaVal[1]);
         }
-        document.title = "Image Pad"
-      }
 
-      // Text Pad
-      if ("text" in aryPara) {
-        document.getElementById("editable").innerText = aryPara[ "text" ];
-        document.title = "Text Pad";
-      }
-
-      // Time Pad
-      if ("countdown" in aryPara) {
-        document.title = "Timer: Countdown";
-        if (aryPara[ "countdown" ] === "undefined") {
-          setTime(0, 0, 0, 30);
+        // Help
+        if ("help" in aryPara) {
+            swal({
+                title: "Help!",
+                text: "<h3>Here are some useful feature:</h3><br>" +
+                "<table>" +
+                "<thead><tr><th>Key</th><th>Value</th><th>Function</th></tr></thead>" +
+                "<tbody>" +
+                "<tr><td>help</td><td>(null)</td><td>Open this page.</td></tr>" +
+                "<tr><td>text</td><td>(text)</td><td>Open an editable pad with (text)</td></tr>" +
+                "<tr><td>timer</td><td>(null)</td><td>Open a timer.</td></tr>" +
+                "<tr><td>timing</td><td>(null)</td><td>Open a timer.</td></tr>" +
+                "<tr><td>countdown</td><td>(?d?h?m?s)</td><td>Open a countdown timer.</td></tr>" +
+                "<tr><td>image</td><td>(url)</td><td>Open an image pad with the specify image.</td></tr>" +
+                "</tbody>" +
+                "</table>",
+                html: true,
+                type: "info",
+                confirmButtonText: "Try it!"
+            });
         } else {
-          let days = aryPara[ "countdown" ].split("d").length > 1 ? aryPara[ "countdown" ].split("d") : [ 0, aryPara[ "countdown" ].split("d")[ 0 ] ];
-          let hours = days[ 1 ].split("h").length > 1 ? days[ 1 ].split("h") : [ 0, days[ 1 ].split("h")[ 0 ] ];
-          let minutes = hours[ 1 ].split("m").length > 1 ? hours[ 1 ].split("m") : [ 0, hours[ 1 ].split("m")[ 0 ] ];
-          let seconds = minutes[ 1 ].split("s")[ 0 ] ? minutes[ 1 ].split("s")[ 0 ] : 0 || 0;
+            // Add feature here
 
-          days = parseInt(days[ 0 ]);
-          hours = parseInt(hours[ 0 ]);
-          minutes = parseInt(minutes[ 0 ]);
-          seconds = parseInt(seconds);
+            // C8763
+            if ("C8763" in aryPara) {
+                let url = "http://i.imgur.com/tqVvt49.gif";
+                document.getElementById("editable").innerHTML = "<img src=\"" + url + "\" style='width: 100%; height: auto;'/>"
+            }
 
-          // Check time over 60s/60m/24h
-          while (seconds >= 60) {
-            minutes += 1;
-            seconds -= 60;
-          }
-          while (minutes >= 60) {
-            hours += 1;
-            minutes -= 60;
-          }
-          while (hours >= 24) {
-            days += 1;
-            hours -= 24;
-          }
+            // Image Pad
+            if ("image" in aryPara) {
+                let imgUrl = decodeURI(aryPara["image"]);
+                imgUrl = imgUrl.replace(/%3A/g, ':').replace(/%2F/g, '/');
+                if (imgUrl != "undefined") {
+                    textArea.innerHTML = "<img src=\"" + imgUrl + "\" style='width: 100%; height: auto;'/>"
+                } else {
+                    textArea.innerText = "404<br>Image Not Found!";
+                }
+                document.title = "Image Pad"
+            }
 
-          setTime(days, hours, minutes, seconds);
+            // Text Pad
+            if ("text" in aryPara) {
+                textArea.innerText = aryPara["text"];
+                document.title = "Text Pad";
+            }
+
+            // Must be the last one
+            if (aryPara["title"]) {
+                document.title = aryPara["title"];
+            }
+
+            // Default Text
+            if (arrPara.size() == 0) {
+                textArea.innerText = "It Works!";
+            }
         }
-        document.getElementById("splitDiv").style.display = "block";
-        start_button.style.display = "inline-block";
-        stop_button.addEventListener('click', function () {
-          start_button.style.display = "inline-block";
-          stop_button.style.display = "none";
-          if (timeInv) {
-            clearInterval(timeInv);
-          }
-        });
-        start_button.addEventListener('click', function () {
-          start_button.style.display = "none";
-          stop_button.style.display = "inline-block";
-          timeInv = setInterval(countdown, 1000);
-        });
-      } else if ("timing" in aryPara) {
-        document.title = "Timer: Timing";
-        setTime(0, 0, 0, 0);
-        document.getElementById("splitDiv").style.display = "block";
-        start_button.style.display = "inline-block";
-        stop_button.addEventListener('click', function () {
-          start_button.style.display = "inline-block";
-          stop_button.style.display = "none";
-          if (timeInv) {
-            clearInterval(timeInv);
-          }
-        });
-        start_button.addEventListener('click', function () {
-          start_button.style.display = "none";
-          stop_button.style.display = "inline-block";
-          timeInv = setInterval(timing, 1000);
-        });
-      } else if ("clock" in aryPara) {
-        document.title = "Clock";
-        showClock();
-      }
-
-      // Must be the last one
-      if (aryPara[ "title" ]) {
-        document.title = aryPara[ "title" ];
-      }
     }
-
-    // FIXME: If not use setTimeout, the "It works" will show before clock.
-    setTimeout(function () {
-      if (document.getElementById("editable").innerText == "Loading...") {
-        document.getElementById("editable").innerText = "It works!";
-      }
-    }, 1000);
-  }
 }
-function editable () {
-  let div = document.getElementById("editable");
-  div.contentEditable = 'true';
-}
-
-function showClock () {
-  setInterval(function () {
-    let currentdate = new Date();
-    document.getElementById("editable").innerHTML =
-      currentdate.getFullYear() + " / "
-      + ("0" + currentdate.getMonth()).slice(-2) + " / "
-      + ("0" + currentdate.getDate()).slice(-2) + "<br>"
-      + ("0" + currentdate.getHours()).slice(-2) + " : "
-      + ("0" + currentdate.getMinutes()).slice(-2) + " : "
-      + ("0" + currentdate.getSeconds()).slice(-2);
-  }, 1000);
-}
-
-function showCountdown (days, hours, mins, secs) {
-  let target = document.getElementById("editable");
-  target.innerText = "";
-  if (days == 0 && hours == 0 && mins == 0 && secs == 0) {
-    target.innerText = "Time's up!";
-    document.title = "Timer: Time's up!";
-    document.getElementById("splitDiv").style.display = "none";
-    document.getElementById("stop").click();
-    document.getElementById("start").style.display = "none";
-  } else {
-    setTime(days, hours, mins, secs);
-  }
-}
-
-function countdown () {
-  let target = document.getElementById("editable");
-  let d = target.innerHTML.split("day(s), ").length > 1 ? target.innerHTML.split("day(s), ") : [ 0, target.innerText.split("day(s), ")[ 0 ] ];
-  let time = d[ 1 ].split(" : ");
-  d = d[ 0 ];
-  let s = time[ 2 ];
-  let m = time[ 1 ];
-  let h = time[ 0 ];
-  d = parseInt(d || 0);
-  h = parseInt(h || 0);
-  m = parseInt(m || 0);
-  s = parseInt(s || 0);
-
-  // Check time over 60s/60m/24h
-  while (s >= 60) {
-    m += 1;
-    s -= 60;
-  }
-  while (m >= 60) {
-    h += 1;
-    m -= 60;
-  }
-  while (h >= 24) {
-    d += 1;
-    h -= 24;
-  }
-
-  if (s > 0 || m > 0 || h > 0 || d > 0) {
-    s -= 1;
-  }
-  if (s < 0) {
-    if (m > 0) {
-      m -= 1;
-      s += 60;
-    } else {
-      if (h > 0) {
-        h -= 1;
-        m += 59;
-        s += 60;
-      } else {
-        if (d > 0) {
-          d -= 1;
-          h += 23;
-          m += 59;
-          s += 60;
-        } else {
-          d = 0;
-          h = 0;
-          m = 0;
-          s = 0;
-        }
-      }
-    }
-  }
-  showCountdown(d, h, m, s);
-}
-
-function timing () {
-  let target = document.getElementById("editable");
-  let data = target.innerHTML.split(" : ");
-  let s = parseInt(data[ 2 ]);
-  let m = parseInt(data[ 1 ]);
-  let h = parseInt(data[ 0 ]);
-
-  // Check time over 60s/60m/24h
-  while (s >= 60) {
-    m += 1;
-    s -= 60;
-  }
-  while (m >= 60) {
-    h += 1;
-    m -= 60;
-  }
-  while (h >= 24) {
-    d += 1;
-    h -= 24;
-  }
-
-  s += 1;
-  if (s >= 60) {
-    s -= 60;
-    m += 1;
-    if (m >= 60) {
-      m -= 60;
-      h += 1;
-      if (h >= 24) {
-        h -= 24;
-      }
-    }
-  }
-  setTime(0, h, m, s);
-}
-
-function setTime (days, hours, mins, secs) {
-  let target = document.getElementById("editable");
-  target.innerHTML = "";
-  if (days > 0) {
-    target.innerHTML = days + "day(s), "
-  }
-  if (hours >= 0) {
-    target.innerHTML += ("0" + hours).slice(-2) + " : ";
-  }
-  if (mins >= 0) {
-    target.innerHTML += ("0" + mins).slice(-2) + " : ";
-  }
-  if (secs >= 0) {
-    target.innerHTML += ("0" + secs).slice(-2);
-  }
+function editable() {
+    let div = document.getElementById("editable");
+    div.contentEditable = 'true';
 }
