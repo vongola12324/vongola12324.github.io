f9b45d7649e53dab8a490e37cba373e86f3b2b08
Vongola <vongola12324@coder.tw>
2018-04-08 14:45:38 +0800

    Reinit project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <router-view/>
+        </div>
+      </div>
+      <div class="hero-foot">
+        <div class="container">
+          <p class="has-text-centered">Copyright &copy; 2018 Vongola</p>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .hero-foot {
+    background-color: dimgrey;
+    color: white;
+    padding-top: 10px !important;
+    padding-bottom: 10px !important;
+  }
+</style>
